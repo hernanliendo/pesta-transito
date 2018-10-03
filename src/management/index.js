377e95679f22092894b2828d29e5055ee3620dfc
hernanliendo <hernan.liendo@gmail.com>
2018-10-03 19:15:39 -0300

    management

--- src/management/index.js
+++ src/management/index.js
@@ -3,6 +3,7 @@
 const admin = require("firebase-admin");
 const serviceAccount = require("./pesta-transito-firebase-adminsdk-twwqq-ca6d8c9df5.json");
 const _ = require('lodash');
+const moment = require('moment');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -28,21 +29,23 @@ const getFamilies = () => db.ref('2018').on('value', snapshot => {
     console.info(r);
 });
 
-const getVoluntarios = () => db.ref('2018').on('value', snapshot => {
-    const model = snapshot.val();
-    let r = '';
+const getVoluntarios = () => db.ref('events').on('value', snapshot => {
+    const evtTypes = ['request', 'changeStatus', 'delivered'];
+    let aggregation = {};
 
-    _.toPairs(model.cars).forEach(p => {
-        const family = model.families[p[1]];
+    _.toPairs(snapshot.val())
+        .filter(p => _.find(evtTypes, t => t === p[1].t))
+        .forEach(p => {
+            const evt = p[1];
+            const date = moment(evt.ets).format('DD-MM-YYYY');
 
-        r += p[0] + ';' + family.n + ';';
+            if (!aggregation[date])
+                aggregation[date] = {};
 
-        _.toPairs(family.ds).forEach(p => r += p[0] + ';' + p[1] + ';');
+            aggregation[date][evt.displayName] = 1;
+        });
 
-        r += '\n';
-    });
-
-    console.info(r);
+    _.toPairs(aggregation).forEach(p => console.warn(p[0] + ';' + Object.keys(p[1]).length + ';' + (_.toPairs(p[1]).map(pp => pp[0] + ';'))));
 });
 
 // getFamilies();
