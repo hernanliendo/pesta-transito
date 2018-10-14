82ac47503166857875d3b526123f9129a7168abb
hernanliendo <hernan.liendo@gmail.com>
2018-10-14 08:45:27 -0300

    grade division

--- src/AddNewCar.js
+++ src/AddNewCar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {Button, SelectField, TextField} from 'react-md';
+import {Button, SelectField, Subheader, TextField} from 'react-md';
 
 const wait = require('wait-promise');
 const _ = require('lodash');
@@ -32,24 +32,30 @@ const TEXTS = {
     newPlate: 'Patente',
     familyName: 'Apellido Familia',
 
-    driver0: '1er Conductor',
-    driverType0: 'Relación 1er Cond.',
-    driver1: '2do Conductor',
-    driverType1: 'Relación 2do Cond.',
+    driver0: 'Nombre',
+    driverType0: 'Relación',
+    driver1: 'Nombre',
+    driverType1: 'Relación',
 
-    student0: 'Chica/o 1',
-    grade0: 'Grado Chica/o 1',
-    student1: 'Chica/o 2',
-    grade1: 'Grado Chica/o 2',
-    student2: 'Chica/o 3',
-    grade2: 'Grado Chica/o 3',
-    student3: 'Chica/o 4',
-    grade3: 'Grado Chica/o 4',
+    student0: 'Nombre',
+    student1: 'Nombre',
+    student2: 'Nombre',
+    student3: 'Nombre',
+
+    grade_grade0: 'Grado',
+    div_grade0: 'División',
+
+    grade: 'Grado',
+    division: 'División',
 
     wsapp0: 'WhatsApp 1',
     wsapp1: 'WhatsApp 2',
     wsapp2: 'WhatsApp 3'
 };
+
+const GRADES = ['Sala 2', 'Sala 3', 'Sala 4', 'Sala 5', '1ro', '2do', '3ro', '4to', '5to', '6to', '7mo'];
+
+const DIVISIONS = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
 
 class AddNewCar extends React.Component {
 
@@ -80,7 +86,10 @@ class AddNewCar extends React.Component {
 
             _.toPairs(f.ks).forEach(p => {
                 s['student' + index] = p[0];
-                s['grade' + index] = p[1];
+
+                const split = p[1].split('_');
+                s['grade_grade' + index] = split[0];
+                s['div_grade' + index] = split[1];
                 index++;
             });
 
@@ -141,6 +150,32 @@ class AddNewCar extends React.Component {
         />;
     }
 
+    renderGrade(id) {
+        return <div>
+            <SelectField
+                id={'grade_' + id}
+                ref={el => this['grade_' + id] = el}
+                label={TEXTS['grade']}
+                onChange={v => this.inputChanged(v, 'grade_' + id)}
+                value={this.state['grade_' + id]}
+                className="md-cell"
+                menuItems={GRADES}
+                simplifiedMenu={true}
+            />
+
+            <SelectField
+                id={'div_' + id}
+                ref={el => this['div_' + id] = el}
+                label={TEXTS['division']}
+                onChange={v => this.inputChanged(v, 'div_' + id)}
+                value={this.state['div_' + id]}
+                className="md-cell"
+                menuItems={DIVISIONS}
+                simplifiedMenu={true}
+            />
+        </div>;
+    }
+
     plateChanged(t) {
         const v = t.replace(/[^a-z0-9]/gi, '').toUpperCase();
 
@@ -171,7 +206,7 @@ class AddNewCar extends React.Component {
     }
 
     dataConfirmed() {
-        this.checkMandatory(['newPlate', 'familyName', 'driver0', 'student0', 'grade0']).then(problems => {
+        this.checkMandatory(['newPlate', 'familyName', 'driver0', 'student0', 'grade_grade0', 'div_grade0']).then(problems => {
             if (problems) return;
 
             const data = this.state;
@@ -179,7 +214,7 @@ class AddNewCar extends React.Component {
             ds[data.driver0] = data.driverType0;
 
             let ks = {};
-            ks[data.student0] = data.grade0;
+            ks[data.student0] = `${data.grade_grade0}_${data.div_grade0}`;
 
             let r = {
                 ds: ds,
@@ -188,16 +223,28 @@ class AddNewCar extends React.Component {
             };
 
             if (data.driver1) ds[data.driver1] = data.driverType1;
-            if (data.student1 && data.grade1) ks[data.student1] = data.grade1;
-            if (data.student2 && data.grade2) ks[data.student2] = data.grade2;
-            if (data.student3 && data.grade3) ks[data.student3] = data.grade3;
+            if (data.student1 && data.grade_grade1) ks[data.student1] = `${data.grade_grade1}_${data.div_grade1}`;
+            if (data.student2 && data.grade_grade2) ks[data.student2] = `${data.grade_grade2}_${data.div_grade2}`;
+            if (data.student3 && data.grade_grade3) ks[data.student3] = `${data.grade_grade3}_${data.div_grade3}`;
 
-            r.wsapp0 = _.get(data, 'wsapp0', null);
-            r.wsapp1 = _.get(data, 'wsapp1', null);
-            r.wsapp2 = _.get(data, 'wsapp2', null);
+            r.wsapp0 = AddNewCar.resolveWhatsApp('wsapp0', data);
+            r.wsapp1 = AddNewCar.resolveWhatsApp('wsapp1', data);
+            r.wsapp2 = AddNewCar.resolveWhatsApp('wsapp2', data);
 
             this.props.onConfirmed({family: r, car: data.newPlate, familyId: data.familyId});
         });
+    }
+
+    static resolveWhatsApp(id, data) {
+        const r = _.get(data, id, null);
+
+        if (r && !r.startsWith('5411')) return '5411' + r;
+
+        return r;
+    }
+
+    static title(t) {
+        return <Subheader style={{listStyleType: 'none', fontSize: '22px', marginTop: '60px'}} primary primaryText={t}/>;
     }
 
     render() {
@@ -220,24 +267,35 @@ class AddNewCar extends React.Component {
                 </div>
 
                 {this.renderInput('familyName')}
+
+
+                {AddNewCar.title('Primer Conductor')}
+
                 {this.renderInput('driver0')}
                 {this.renderSelect('driverType0')}
 
+
+                {AddNewCar.title('Segundo Conductor')}
                 {this.renderInput('driver1')}
                 {this.renderSelect('driverType1')}
 
+                {AddNewCar.title('Primer Alumno')}
                 {this.renderInput('student0')}
-                {this.renderInput('grade0')}
+                {this.renderGrade('grade0')}
 
+                {AddNewCar.title('Segundo Alumno')}
                 {this.renderInput('student1')}
-                {this.renderInput('grade1')}
+                {this.renderGrade('grade1')}
 
+                {AddNewCar.title('Tercer Alumno')}
                 {this.renderInput('student2')}
-                {this.renderInput('grade2')}
+                {this.renderGrade('grade2')}
 
-                {this.renderInput('grade3')}
+                {AddNewCar.title('Cuarto Alumno')}
                 {this.renderInput('student3')}
+                {this.renderGrade('grade3')}
 
+                {AddNewCar.title('Números WhatsApp')}
                 {this.renderInput('wsapp0', true)}
                 {this.renderInput('wsapp1', true)}
                 {this.renderInput('wsapp2', true)}
